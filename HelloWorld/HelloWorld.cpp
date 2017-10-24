ac51482d624fee40fccd1abc4d29ecbecd6c5789
Cameron Mathis <phyluxity@gmail.com>
2017-10-24 12:10:56 -0500

    Update HelloWorld.cpp

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -16,6 +16,7 @@ void Census2017(){
   Census.push_back("Rahultheman12 @ https://github.com/Rahultheman12");
   Census.push_back("spicyboi @ https://github.com/spicyboi");
   Census.push_back("John Nguyen @ https://github.com/jawnlovesfreestuff");
+  Census.push_back("Cameron Mathis @ https://github.com/Phylux");
 
 }
 
