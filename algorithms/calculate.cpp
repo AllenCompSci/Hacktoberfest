930a04fc697394b8fe183c10e6c97b7d5003d8fa
Taylor Hudson <theshrewedshrew@gmail.com>
2017-10-25 16:22:40 -0500

    Create calculate.cpp

---
+++ algorithms/calculate.cpp
@@ -0,0 +1,14 @@
+// AUTHOR : theshrewedshrew @ https://github.com/theshrewedshrew
+// REASON : Create a program, and or series of cpp files capable of calculating any two numbers.
+// NEEDS  : ADD() SUB() MULTI() DIVIDE() MOD()
+
+#include <iostream>
+#incldue <string>
+
+using namespace std;
+
+
+void main(){
+
+
+}
