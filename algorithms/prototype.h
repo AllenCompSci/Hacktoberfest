365fc88bd6ee7c22649e55d6c4930280ed66578a
Texas Snow <TexasSnow75402@gmail.com>
2017-10-26 05:44:04 -0500

    Update prototype.h

--- algorithms/prototype.h
+++ algorithms/prototype.h
@@ -6,6 +6,7 @@ std::string SUB(std::string, std::string);
 std::string DIVIDE(std::string, std::string);
 std::string MULTI(std::string, std::string);
 std::string MOD(std::string, std::string);
+std::string gcd(std::string, std::string);
 
 bool isLessThan(std::string, std::string);
 bool isEqualTo(std::string, std::string);
