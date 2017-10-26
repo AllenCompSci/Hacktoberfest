680a5c332f54753100497d19fdffa5d73a154fe0
Taylor Hudson <theshrewedshrew@gmail.com>
2017-10-25 19:04:56 -0500

    Update algorithm-string.h

--- algorithms/algorithm-string.h
+++ algorithms/algorithm-string.h
@@ -1,2 +1,50 @@
 #include "prototype.h"
 
+std::string ADD(std::string, std::string){
+  
+}
+std::string SUB(std::string, std::string){
+  
+}
+std::string DIVIDE(std::string, std::string){
+  
+}
+std::string MULTI(std::string, std::string){
+  
+}
+std::string MOD(std::string, std::string){
+  
+}
+
+bool isLessThan(std::string x, std::string y){
+  if(x.length() > 0 && x[0] == '-'){
+   if(y.length() > 0 && y[0] == '-'){
+       
+   }
+    else{
+      return true;
+    }
+  }
+  else if (y.length() > 0 && y[0] == '-'){
+    return false; 
+  }
+  if((int)x.length() == (int)y.length()){
+    for(int i = 0; i < (int)x.length(); i++)
+      if(x[i] < y[i]){
+       return true;
+      }
+    else if(x[i] != y[i]{
+      return false;
+    }
+  }
+  else if((int)x.length() < (int)y.length()){
+       return true;
+  }
+  return false;
+}
+bool isEqualTo(std::string x, std::string y){
+    return x == y;
+}
+bool isGreaterThan(std::string, std::string){
+  return !isEqualtTo(x,y) && !isLessThan(x,y);
+}
