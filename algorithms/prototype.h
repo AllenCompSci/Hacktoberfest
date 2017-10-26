4c23af5225dc66b633ad54c91917631d528c7974
Texas Snow <TexasSnow75402@gmail.com>
2017-10-26 07:41:16 -0500

    Update prototype.h

--- algorithms/prototype.h
+++ algorithms/prototype.h
@@ -14,6 +14,7 @@ int cmprLen(std::string, std::string);
 std::string ADD(std::string, std::string);
 std::string SUB(std::string, std::string);
 std::string DIVIDE(std::string, std::string);
+std::string INTDIV(std::string, std::string);
 std::string MULTI(std::string, std::string);
 std::string MOD(std::string, std::string);
 std::string gcd(std::string, std::string);
