1aa3ac741cb57b587f5427c7d32d308a2ad6bfd3
Maxwell Cody <maxwell@cody.sh>
2017-10-28 01:09:58 -0500

    Cleanliness

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -7,6 +7,7 @@ class HelloWorld {
     /*
      * Add yourself using the following format:
      *    CENSUS.add("Your Name @ https://github.com/username");
+     *
      */
     CENSUS.add("Allen Comp Sci @ https://github.com/AllenCompSci");
     CENSUS.add("Mr. Hudson @ https://github.com/theshrewedshrew");
